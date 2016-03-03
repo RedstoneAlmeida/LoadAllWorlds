8b8b197faa79a5ee6a93a0dc83ece835a4e2af82
Driesboy <coenendries3@gmail.com>
2016-03-03 07:12:14 +0100

    Create main.php

---
+++ src/driesboy/main.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace driesboy;
+
+use pocketmine\event\level\LevelLoadEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+
+
+class Main extends PluginBase implements Listener{
+
+	public function onEnable() {
+	$this->getServer()->getLogger()->info(TextFormat::BLUE . "LoadAllWorlds Has Been Enabled.");
+	$this->getServer()->getLogger()->info(TextFormat::BLUE . "By: Driesboy. http://github.com/Driesboy");
+	$this->getServer()->loadLevel(Void Of death);
+	$this->getServer()->loadLevel(world);
+	$this->getServer()->loadLevel(plot);
+	$this->getServer()->loadLevel(Lobby);
+	}
+	}
