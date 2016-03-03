4c4b7db76cff7f83e47edc0258d64f300a443a74
Driesboy <coenendries3@gmail.com>
2016-03-03 16:48:54 +0100

    Update Main.php

--- src/driesboy/Main.php
+++ src/driesboy/Main.php
@@ -12,7 +12,7 @@ class Main extends PluginBase implements Listener{
 	public function onEnable() {
 	$this->getServer()->getLogger()->info(TextFormat::BLUE . "LoadAllWorlds Has Been Enabled.");
 	$this->getServer()->getLogger()->info(TextFormat::BLUE . "By: Driesboy. http://github.com/Driesboy");
-	$this->getServer()->loadLevel(Void Of death);
+	$this->getServer()->loadLevel(Void_Of_death);
 	$this->getServer()->loadLevel(world);
 	$this->getServer()->loadLevel(plot);
 	$this->getServer()->loadLevel(Lobby);
